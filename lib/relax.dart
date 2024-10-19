316fa075f43ac68be2753c2a124a5db529ce21b2
IvaFraMa <im2021071086@virtual.upt.pe>
2024-10-19 14:32:42 -0500

    relax

--- lib/relax.dart
+++ lib/relax.dart
@@ -1,47 +1,97 @@
 import 'package:flutter/material.dart';
-import 'ruta.dart';
-import 'juegos.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class RelaxScreen extends StatelessWidget {
+  final CarouselSliderController _carouselController =
+      CarouselSliderController(); // Usar la clase correcta
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
+        title: const Text('Relax'),
+        backgroundColor: const Color(0xFF406E5B),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Pantalla de Dashboard (Modo Desconectado)'),
-            SizedBox(height: 20), // Espacio entre el texto y los botones
-            ElevatedButton(
-              onPressed: () {
-                // Navegar a la pantalla definida en ruta.dart
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          RutaScreen()), // Asegúrate de que RutaScreen esté definido en ruta.dart
-                );
-              },
-              child: Text('Ir a Ruta'),
+      backgroundColor: const Color(0xFFFBE4CF),
+      body: Column(
+        children: [
+          Container(
+            color: const Color(0xFF406E5B),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: const [
+                Text(
+                  'RELAX',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'TARATA',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFFFADFAE),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20), // Espacio entre los botones
-            ElevatedButton(
-              onPressed: () {
-                // Navegar a la pantalla definida en juegos.dart
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          ParqueFerialScreen()), // Asegúrate de que JuegosScreen esté definido en juegos.dart
-                );
-              },
-              child: Text('Ir a Juegos'),
+          ),
+          const SizedBox(height: 20),
+
+          // Primer carrusel
+          CarouselSlider(
+            carouselController: _carouselController, // Clase corregida
+            options: CarouselOptions(
+              height: 200,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              enlargeCenterPage: true,
             ),
-          ],
-        ),
+            items: [
+              'assets/images/relax1.png',
+              'assets/images/relax2.png',
+              'assets/images/relax3.png',
+            ]
+                .map((item) => Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(item),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ))
+                .toList(),
+          ),
+          const SizedBox(height: 20),
+
+          // Segundo carrusel
+          CarouselSlider(
+            carouselController: _carouselController, // Clase corregida
+            options: CarouselOptions(
+              height: 200,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              enlargeCenterPage: true,
+            ),
+            items: [
+              'assets/images/relax4.png',
+              'assets/images/relax5.png',
+              'assets/images/relax6.png',
+            ]
+                .map((item) => Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(item),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ))
+                .toList(),
+          ),
+        ],
       ),
     );
   }

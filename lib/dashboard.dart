fc23221ad79a3a21d60a946bf2e10a07e843d4b3
IvaFraMa <im2021071086@virtual.upt.pe>
2024-10-19 08:37:39 -0500

    dashboard principal

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,40 +1,205 @@
 import 'package:flutter/material.dart';
-import 'ruta.dart'; 
+import 'ruta.dart';
 import 'juegos.dart';
+import 'relax.dart';
+import 'senderismo.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreen createState() => _DashboardScreen();
+}
+
+class _DashboardScreen extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
+        title: const Text('Dashboard'),
       ),
-      body: Center(
+      backgroundColor: const Color(0xFFFBE4CF),
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Pantalla de Dashboard (Modo Desconectado)'),
-            SizedBox(height: 20), // Espacio entre el texto y los botones
-            ElevatedButton(
-              onPressed: () {
-                // Navegar a la pantalla definida en ruta.dart
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RutaScreen()), // Asegúrate de que RutaScreen esté definido en ruta.dart
-                );
-              },
-              child: Text('Ir a Ruta'),
+            // Parte superior con la imagen y el título
+            Container(
+              height: 350, // Ajusta la altura para que la imagen sea más grande
+              width: double.infinity, // Ocupa todo el ancho de la pantalla
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const CircleAvatar(
+                    radius: 60, // Imagen del usuario más grande
+                    backgroundImage: AssetImage('assets/images/user.png'),
+                    backgroundColor: Colors.transparent,
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Bienvenido Usuario',
+                    style: TextStyle(
+                      fontSize: 32, // Aumentar el tamaño del texto
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 20), // Espacio entre los botones
-            ElevatedButton(
-              onPressed: () {
-                // Navegar a la pantalla definida en juegos.dart
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ParqueFerialScreen()), // Asegúrate de que JuegosScreen esté definido en juegos.dart
-                );
-              },
-              child: Text('Ir a Juegos'),
+            const SizedBox(height: 20), // Espacio entre el texto y los botones
+
+            // Fila de botones
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => RelaxScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF406E5B), // Color de fondo
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Ir a Relax',
+                          style:
+                              TextStyle(color: Colors.white), // Color de texto
+                        ),
+                        const SizedBox(
+                            height: 8), // Espacio entre el texto y el ícono
+                        Image.asset(
+                          'assets/images/relax.png', // Ruta del ícono
+                          height: 24, // Tamaño del ícono
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10), // Espacio entre botones
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SenderismoScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF406E5B),
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Ir a Senderismo',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 8),
+                        Image.asset(
+                          'assets/images/senderismo.png',
+                          height: 24,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10), // Espacio entre las filas
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => RutaScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF406E5B),
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Ir a Ruta',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 8),
+                        Image.asset(
+                          'assets/images/ruta.png',
+                          height: 24,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ParqueFerialScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF406E5B),
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Ir a Juegos',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 8),
+                        Image.asset(
+                          'assets/images/yunga.png',
+                          height: 24,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20), // Espacio entre las filas
+
+            // Botón de Cerrar Sesión
+            SizedBox(
+              width: double.infinity, // Ocupa todo el ancho
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // Cierra sesión y regresa
+                },
+                child: const Text('Cerrar Sesión'),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  backgroundColor: Colors.redAccent, // Color del botón
+                ),
+              ),
             ),
           ],
         ),
---
+++ lib/senderismo.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'ruta.dart';
+import 'juegos.dart';
+
+class SenderismoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Pantalla de Dashboard (Modo Desconectado)'),
+            SizedBox(height: 20), // Espacio entre el texto y los botones
+            ElevatedButton(
+              onPressed: () {
+                // Navegar a la pantalla definida en ruta.dart
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          RutaScreen()), // Asegúrate de que RutaScreen esté definido en ruta.dart
+                );
+              },
+              child: Text('Ir a Ruta'),
+            ),
+            SizedBox(height: 20), // Espacio entre los botones
+            ElevatedButton(
+              onPressed: () {
+                // Navegar a la pantalla definida en juegos.dart
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          ParqueFerialScreen()), // Asegúrate de que JuegosScreen esté definido en juegos.dart
+                );
+              },
+              child: Text('Ir a Juegos'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

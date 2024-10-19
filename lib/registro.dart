cd6b94d1f3c1c13c5d9be41fff350aa736229ec3
ximena-ortiz <xo2021071080@virtual.upt.pe>
2024-10-18 21:13:21 -0500

    pagina de inicio app tarata

---
+++ lib/registro.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistroScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro'),
+      ),
+      body: Center(
+        child: Text('Pantalla de Registro'),
+      ),
+    );
+  }
+}

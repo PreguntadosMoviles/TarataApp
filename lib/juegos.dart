01b79b709d9eaf47b0d091ce1dd0a417270781e7
ximena-ortiz <102675516+ximena-ortiz@users.noreply.github.com>
2024-10-22 17:51:26 -0500

    Update juegos.dart

--- lib/juegos.dart
+++ lib/juegos.dart
@@ -24,11 +24,11 @@ class ParqueFerialScreen extends StatelessWidget {
           Positioned.fill(
             child: ColorFiltered(
               colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.5), // Oscurece la imagen
+                Colors.black.withOpacity(0.5),
                 BlendMode.darken,
               ),
               child: Image.asset(
-                'assets/images/puente.jpg', // Imagen del puente tibetano
+                'assets/images/puente.jpg', 
                 fit: BoxFit.cover,
               ),
             ),
@@ -36,7 +36,7 @@ class ParqueFerialScreen extends StatelessWidget {
           // Contenido superpuesto
           Column(
             children: [
-              // Título "YUNGA" y "PARQUE FERIAL"
+              
               Padding(
                 padding: const EdgeInsets.only(top: 40.0),
                 child: Text(
@@ -52,28 +52,27 @@ class ParqueFerialScreen extends StatelessWidget {
               Text(
                 'PARQUE FERIAL',
                 style: TextStyle(
-                  fontSize: 32, // Aumenta el tamaño del texto
+                  fontSize: 32, 
                   fontWeight: FontWeight.bold,
-                  color: Color(0xFFFFE135), // Amarillo patito
+                  color: Color(0xFFFFE135),
                   letterSpacing: 2,
                 ),
               ),
               Spacer(),
-              // Título "PUENTE TIBETANO" con flechas a los lados
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Icon(Icons.arrow_back_ios,
                       color: Colors.white,
                       size: 48,
-                      weight: 5), // Flechas más grandes y gruesas
+                      weight: 5), 
                   Column(
                     children: [
                       Text(
                         'PUENTE',
                         style: TextStyle(
                           fontSize: 36,
-                          fontWeight: FontWeight.bold, // Engrosar el texto
+                          fontWeight: FontWeight.bold, 
                           color: Colors.white,
                         ),
                       ),
@@ -81,7 +80,7 @@ class ParqueFerialScreen extends StatelessWidget {
                         'TIBETANO',
                         style: TextStyle(
                           fontSize: 36,
-                          fontWeight: FontWeight.bold, // Engrosar el texto
+                          fontWeight: FontWeight.bold, 
                           color: Colors.white,
                         ),
                       ),
@@ -90,22 +89,21 @@ class ParqueFerialScreen extends StatelessWidget {
                   Icon(Icons.arrow_forward_ios,
                       color: Colors.white,
                       size: 48,
-                      weight: 5), // Flechas más grandes y gruesas
+                      weight: 5), 
                 ],
               ),
               SizedBox(height: 10),
-              // Sección gris oscura con bordes redondeados, texto y botón "Más Información"
               Container(
                 decoration: BoxDecoration(
                   color: Colors.grey[900],
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0), // Bordes redondeados
+                    topLeft: Radius.circular(30.0), 
                     topRight: Radius.circular(30.0),
                   ),
                 ),
                 width: double.infinity,
                 height: MediaQuery.of(context).size.height *
-                    0.3, // Ajuste de altura para 1/4 de la pantalla
+                    0.3, 
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -121,7 +119,6 @@ class ParqueFerialScreen extends StatelessWidget {
                     SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: () {
-                        // Acción del botón "Más Información"
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.green,
@@ -143,14 +140,14 @@ class ParqueFerialScreen extends StatelessWidget {
           Positioned(
             left: 10,
             top: MediaQuery.of(context).size.height *
-                0.4, // Ajustado para estar más arriba
+                0.4, 
             child: Column(
               children: [
                 IconButton(
                   icon: Icon(Icons.attach_money,
-                      color: Colors.white, size: 48), // Icono más grande
+                      color: Colors.white, size: 48), 
                   onPressed: () {
-                    // Acción del botón de precios
+                   
                   },
                 ),
                 Text(
@@ -163,14 +160,14 @@ class ParqueFerialScreen extends StatelessWidget {
           Positioned(
             right: 10,
             top: MediaQuery.of(context).size.height *
-                0.4, // Ajustado para estar más arriba
+                0.4, 
             child: Column(
               children: [
                 IconButton(
                   icon: Icon(Icons.photo,
-                      color: Colors.white, size: 48), // Icono más grande
+                      color: Colors.white, size: 48), 
                   onPressed: () {
-                    // Acción del botón de galería
+                   
                   },
                 ),
                 Text(
@@ -184,12 +181,11 @@ class ParqueFerialScreen extends StatelessWidget {
       ),
       floatingActionButton: Positioned(
         left: 20,
-        top: 40, // Posiciona el botón en la parte superior izquierda
+        top: 40, 
         child: FloatingActionButton(
           backgroundColor: Colors.green,
           child: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
-            // Navegar a la pantalla definida en juegos.dart
             Navigator.pop(context);
           },
         ),
